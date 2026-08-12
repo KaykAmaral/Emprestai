3cc8f99d4514abe66dc2cf391fd5e66072d6d372
Kayk Eduardo Stefano Amaral <kayaquiurbano@gmail.com>
2026-08-12 14:03:22 -0300

    Backend - Initial Commit
    The basic of Spring project.
    Added all project's dependecies btw

---
+++ emprestai_backend/src/test/java/com/example/emprestai/EmprestaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emprestai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprestaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
